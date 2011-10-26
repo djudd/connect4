3e5e1c48a0ac75894ac44b5072bfb69157e94186
David Judd <djudd@yodle.com>
2011-10-26 11:08:08 -0400

    fix server for new version of game host, and add python 3 version of server

--- server3.py
+++ server3.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import simplejson, sys, time
+import json, sys
 from subprocess import check_output
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 global engine_path
 global millis_per_move
@@ -23,7 +23,7 @@ class MyHandler(BaseHTTPRequestHandler):
             self.rfile.readline()
             post_body = self.rfile.readline()
 
-        data = simplejson.loads(post_body)
+        data = json.loads(post_body.decode())
 
         move = get_move(data)  
 
@@ -31,7 +31,8 @@ class MyHandler(BaseHTTPRequestHandler):
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
 
-        self.wfile.write('{"move":%d}' % move)
+        out = '{"move":%d}' % move
+        self.wfile.write(out.encode())
 
 def get_move(data):
     set_prev_moves(data)
@@ -43,8 +44,8 @@ def find_best_move():
     global moves
     global millis_per_move
     args = [engine_path, millis_per_move] + moves
-    out = check_output(map(str, args)) 
-    assert(out.count("\n") == 1) 
+    out = check_output(map(str, args))
+    assert(out.decode().count("\n") == 1) 
     return int(out)    
 
 def set_prev_moves(data):
@@ -54,15 +55,15 @@ def set_prev_moves(data):
     if data["moveNumber"] > 0:
         moves.append(data["lastMove"]["col"])
 
-    print " ".join(str(move) for move in moves)
+    print(" ".join(str(move) for move in moves))
 
 def serve(port):
     try:
         server = HTTPServer(('',port), MyHandler)
-        print 'Starting server...'
+        print('Starting server...')
         server.serve_forever()
     except KeyboardInterrupt:
-        print '^C received, shutting down server...'
+        print('^C received, shutting down server...')
         server.socket.close()
 
 def validate_and_store_engine(engine):
@@ -72,7 +73,7 @@ def validate_and_store_engine(engine):
     try:
         find_best_move()
     except:
-        print "Engine executable at %s does not exist or is not valid" % engine
+        print("Engine executable at %s does not exist or is not valid" % engine)
         exit(-1)
 
 def validate_and_store_millis(millis):
@@ -87,7 +88,7 @@ if __name__ == '__main__':
         port = int(sys.argv[2])
         millis = int(sys.argv[3])
     except:
-        print "Usage: server.py <path_to_engine> <port> <max_millis_per_move>"    
+        print("Usage: server.py <path_to_engine> <port> <max_millis_per_move>")
         exit(-1)
 
     validate_and_store_millis(millis)
